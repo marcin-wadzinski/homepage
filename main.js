5ffc69ffe537d28fc6734442c9fdaee9cf48d8ce
Marcin Wadziński <mwadzinski@icloud.com>
2020-05-26 20:40:56 +0200

    addition of BEM

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const firstName = 'Marcin';
-const surName = 'Wadziński';
-const age = '42';
-const hobby = 'programowanie';
-
-
+const firstName = "Marcin";
+const surName = "Wadziński";
+const age = "42";
+const hobby = "programowanie";
 
 console.log(firstName);
 console.log(surName);
 console.log(age);
 console.log(hobby);
-console.log(`Siema na imię mam ${firstName} ,a na nazwisko ${surName}i mam ${age} lat,a moje hobby to ${hobby}`);
+console.log(
+  `Siema na imię mam ${firstName} ,a na nazwisko ${surName}i mam ${age} lat,a moje hobby to ${hobby}`
+);
